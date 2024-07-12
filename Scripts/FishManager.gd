faf3474757cbe1618425a09b69d8fc210e689197
GCidon <cidonchap@gmail.com>
2024-07-12 15:18:31 +0200

    Mejor calculo

--- Scripts/FishManager.gd
+++ Scripts/FishManager.gd
@@ -8,7 +8,7 @@ extends Node
 var totalFishCaught : int = 0
 var distinctFishCaught : int = 0
 
-var fishDictionary : Dictionary
+var fishArray : Array
 
 func _ready():
 	for i in totalNumFish:
@@ -17,9 +17,8 @@ func _ready():
 			newfish = load("res://Resources/Fishes/FishColosal.tres") as Fish
 		else:
 			newfish = load("res://Resources/Fishes/Fish"+str(i+1)+".tres") as Fish
-		newfish.Probability = getGaussianProbability(i)
-		fishDictionary[newfish.Name] = newfish
-		print("Loaded " + newfish.Name + " with prob " + str(newfish.Probability))
+		fishArray.push_back(newfish)
+	recalculateProbabilities()
 		
 func getGaussianProbability(num):
 	var prob : float = 0.0
@@ -29,3 +28,14 @@ func getGaussianProbability(num):
 	var sec : float = pow(eulerNumber, exponent) 
 	prob = first*sec
 	return prob
+	
+func recalculateProbabilities():
+	for i in totalNumFish:
+		fishArray[i].Probability = getGaussianProbability(i)
+		print(str(fishArray[i].Probability))
+	
+	if distinctFishCaught > totalNumFish-3:
+		fishArray[totalNumFish-1].Probability = 0.1
+	else:
+		fishArray[totalNumFish-1].Probability = 0.0
+		
