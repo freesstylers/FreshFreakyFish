9a03fca483f655e52976ea7fd0891f8b0932a485
GCidon <cidonchap@gmail.com>
2024-07-12 14:59:58 +0200

    Distribución normal para probabilidad de peces

--- Scripts/FishManager.gd
+++ Scripts/FishManager.gd
@@ -1,7 +1,8 @@
 extends Node
 
-
 @export var totalNumFish : int = 13
+
+@export var variance : float = 1.2
 
 var totalFishCaught : int = 0
 var distinctFishCaught : int = 0
@@ -15,6 +16,15 @@ func _ready():
 			newfish = load("res://Resources/Fishes/FishColosal.tres") as Fish
 		else:
 			newfish = load("res://Resources/Fishes/Fish"+str(i+1)+".tres") as Fish
-		newfish.Probability = 0.0
+		newfish.Probability = getGaussianProbability(i)
 		fishDictionary[newfish.Name] = newfish
-		print("Loaded " + newfish.Name)
+		print("Loaded " + newfish.Name + " with prob " + str(newfish.Probability))
+		
+func getGaussianProbability(num):
+	var prob : float = 0.0
+	var first : float = 1/(sqrt(2*PI*variance))
+	var eulerNumber = 2.71828
+	var exponent = -(pow(num-distinctFishCaught, 2)/(2*variance))
+	var sec : float = pow(eulerNumber, exponent) 
+	prob = first*sec
+	return prob
