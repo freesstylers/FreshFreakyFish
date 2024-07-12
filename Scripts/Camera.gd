1adacd12a05c2b9f74dd7756b8f28fa223482ce7
GCidon <cidonchap@gmail.com>
2024-07-12 13:30:39 +0200

    Prueba de mainmenu y juego en misma escena

---
+++ Scripts/Camera.gd
@@ -0,0 +1,19 @@
+extends Camera2D
+
+@export var mainpos : Node2D = null
+@export var gamepos : Node2D = null
+
+var posToMoveTo : Vector2 = Vector2(0,0)
+
+func _ready():
+	transform = mainpos.transform
+	posToMoveTo = mainpos.position
+
+func _on_play_pressed():
+	posToMoveTo = gamepos.position
+	
+func _process(delta):
+	position = lerp(position, posToMoveTo, 0.05)
+
+func _on_back_pressed():
+	posToMoveTo = mainpos.position
