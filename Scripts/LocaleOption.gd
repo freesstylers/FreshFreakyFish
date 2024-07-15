14b7486448c54f26cd4bbe2f77d805c35eaa2e57
IgnOry <37537092+IgnOry@users.noreply.github.com>
2024-07-15 13:13:45 +0200

    LOC fix

--- Scripts/LocaleOption.gd
+++ Scripts/LocaleOption.gd
@@ -25,12 +25,7 @@ func _ready():
 			"en":
 				add_item(TranslationServer.get_locale_name(locale))	
 	
-	if not get_tree().root.get_node("SceneManager").firstTimeLangSetup:
-		select(langs.find("ca"))
-		TranslationServer.set_locale("ca")
-		get_tree().root.get_node("SceneManager").firstTimeLangSetup = true
-	else:
-		select(langs.find(TranslationServer.get_locale()))
+	select(langs.find(TranslationServer.get_locale()))
 
 func _on_item_selected(index):
 	TranslationServer.set_locale(langs[index])
