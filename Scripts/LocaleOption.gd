7fe724af7df71ba084bc1ae4cc00663e217adde6
IgnOry <37537092+IgnOry@users.noreply.github.com>
2024-07-15 12:36:54 +0200

    menu sound

---
+++ Scripts/LocaleOption.gd
@@ -0,0 +1,36 @@
+extends OptionButton
+
+var langs : PackedStringArray
+
+func _ready():
+	langs = TranslationServer.get_loaded_locales()
+	for locale in langs:
+		match locale:	
+			"ca_ES":
+				add_item("Valencià")
+			"ca":
+				add_item("Catalá")
+			"es":
+				add_item("Castellano")
+			"eu":
+				add_item("Euskera")
+			"gl":
+				add_item("Galego")	
+			"ast":
+				add_item("Asturianu")	
+			"la":
+				add_item("Latīnum")	
+			"ang":
+				add_item("Andalûh")
+			"en":
+				add_item(TranslationServer.get_locale_name(locale))	
+	
+	if not get_tree().root.get_node("SceneManager").firstTimeLangSetup:
+		select(langs.find("ca"))
+		TranslationServer.set_locale("ca")
+		get_tree().root.get_node("SceneManager").firstTimeLangSetup = true
+	else:
+		select(langs.find(TranslationServer.get_locale()))
+
+func _on_item_selected(index):
+	TranslationServer.set_locale(langs[index])
---
+++ Scripts/MainMenu.gd
@@ -0,0 +1,53 @@
+extends Control
+
+@onready var ButtonSFX: AudioStreamPlayer = $ButtonSFX
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func FreeStylers():
+	ButtonSFX.play()
+	OS.shell_open("https://freestylers-studio.itch.io/")
+	pass # Replace with function body.
+
+func Jam():
+	ButtonSFX.play()
+	OS.shell_open("https://itch.io/jam/mermelada-jam-3")
+	pass # Replace with function body.
+
+func Twitter():
+	ButtonSFX.play()
+	OS.shell_open("https://twitter.com/FreeStylers_Dev")
+
+
+func _on_freestylers_pressed():
+	FreeStylers()
+
+func _on_jam_pressed():
+	Jam()
+
+func _on_twitter_pressed():
+	Twitter()
+
+
+func _on_play_pressed():
+	pass # Replace with function body.
+
+
+func _on_how_to_play_pressed():
+	pass # Replace with function body.
+
+
+func _on_credits_pressed():
+	pass # Replace with function body.
+
+func _on_LanguageSelector_button_pressed():
+	ButtonSFX.play()
+	pass # Replace with function body.
